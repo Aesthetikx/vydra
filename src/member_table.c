d299b561d6d56c250b6cf2d9059deba36f4fde24
John DeSilva <desilvjo@umich.edu>
2014-09-05 23:07:24 -0400

    Add return statement

--- src/member_table.c
+++ src/member_table.c
@@ -6,4 +6,5 @@ new_member_table(member_table *parent, int size)
   member_table *table = malloc(sizeof(member_table));
   table->parent = parent;
   table->members = new_hash_table(size);
+  return table;
 }
